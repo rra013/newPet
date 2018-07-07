cc8774d7a798ce76ce29bd395d00347e7f7afa16
Rishi Anand <rishi@rishis-macbook-pro.local>
2018-07-07 13:18:50 -0500

    Town skeleton in place

---
+++ newPet/town.swift
@@ -0,0 +1,14 @@
+//
+//  town.swift
+//  newPet
+//
+//  Created by Rishi Anand on 7/7/18.
+//  Copyright © 2018 Rishi Anand. All rights reserved.
+//
+
+import Foundation
+
+class Town{
+    var name = ""
+    var placesAccessible = [Place]()
+}
