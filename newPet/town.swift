41dc8e4a2d3c21280334dbb1d92b00fdea7f82f3
Rishi Anand <rishi@rishis-macbook-pro.local>
2018-07-08 15:06:44 -0500

    Second VC and segues added

--- newPet/town.swift
+++ newPet/town.swift
@@ -11,4 +11,22 @@ import Foundation
 class Town{
     var name = ""
     var placesAccessible = [Place]()
+    init() {
+        let dogPark = Place()
+        placesAccessible.append(dogPark)
+        let play = Action()
+        play.name = "Play"
+        play.caption = "You played with your dog for some time."
+        play.happEffectRandomizer(lowerBound: 25, upperBound: 75)
+        play.hungEffectRandomizer(lowerBound: -50, upperBound: -10)
+        placesAccessible[0].actionsAvailable.append(play)
+        let foodShoppe = Place()
+        placesAccessible.append(foodShoppe)
+        let eatFood = Action()
+        eatFood.name = "Eat"
+        eatFood.caption = "You ate food with your dog for some time."
+        eatFood.happEffectRandomizer(lowerBound: -25, upperBound: 25)
+        eatFood.hungEffectRandomizer(lowerBound: 10, upperBound: 40)
+        placesAccessible[1].actionsAvailable.append(eatFood)
+    }
 }
